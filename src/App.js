48ee99683f42b34edfc10650bf2bdaf9cb94e722
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-09 13:29:58 -0700

    MVP Met I think

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
 
+  const [image, setImage] = useState('')
 
+  useEffect(() => {
+    Axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')//
+    .then(res => {
+      setImage(res.data.url)
+    })
+  }, [])
+
+  const Image = ({ url }) => (
+    <div className='image'>
+      <img src={url} alt={'space'}/>
+    </div>
+  )
   
   return (
     <div className="App">
@@ -11,6 +25,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <Image url={image}/>
     </div>
   );
 }
