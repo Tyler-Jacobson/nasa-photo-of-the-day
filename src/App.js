3e27cf2bc872f1de9744a1346441d52848c27e78
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-10 13:54:16 -0700

    Added Styles to js file. MVP Met

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect }  from "react";
 import "./App.css";
 import Axios from "axios";
+import styled, { keyframes } from 'styled-components'
 
 import Header from './Header'
 
+const today = new Date()
 function App() {
-
+  
   const [image, setImage] = useState('')
 
   useEffect(() => {
-    Axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')//
+    Axios.get('')//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-9-10
     .then(res => {
       setImage(res.data.url)
+    })
+    .catch(err => {
+      console.log(err)
     })
   }, [])
 
@@ -20,7 +25,7 @@ function App() {
       <img src={url} alt={'space'}/>
     </div>
   )
-  
+  console.log(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate())
   return (
     <div className="App">
       <Header />
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./Header.css";
+import styled, { keyframes } from "styled-components";
+
+const ATag = styled.a`
+  color: black;
+  font-size: 1.5rem;
+  border-radius: 2rem;
+  padding: 1rem;
+  margin: 0.5rem;
+  width: 50%;
+  background-color: lightblue;
+`;
 
 export default function Header() {
-
-    return (
-        <div className="App">
-            <div className="spacer">
-                <nav>
-                    <a href="app.js">Home</a>
-                    <span> </span>
-                    <a href="https://www.nasa.gov/multimedia/imagegallery/index.html">More Images</a>
-                    <span> </span>
-                    <a href="https://api.nasa.gov/#apod">API</a>
-                </nav>
-            </div>
+  return (
+    <div className="App">
+      <div className="spacer">
+        <nav>
+          <ATag href="app.js">Home</ATag>
+          <span> </span>
+          <ATag href="https://www.nasa.gov/multimedia/imagegallery/index.html">
+            More Images
+          </ATag>
+          <span> </span>
+          <ATag href="https://api.nasa.gov/#apod">API</ATag>
+        </nav>
       </div>
-    )
+    </div>
+  );
 }
-
